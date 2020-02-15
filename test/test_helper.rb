1f92c18f4bbeaf3e6342f47e3d78ef142d4e1bac
rickbacci <rickbacci@gmail.com>
2020-02-14 18:33:23 -0700

    install minitest and simplecov

---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'csv'
+require 'pry'
+
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# require_relative '../lib/parser'
+# require_relative '../lib/sales_engine'
---
+++ test/test_test.rb
@@ -0,0 +1,8 @@
+require_relative 'test_helper'
+
+class TestTest < Minitest::Test
+
+  def test_it_works
+    assert true
+  end
+end
