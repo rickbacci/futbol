4fb7c3ec6589abf95040f6aab6a6554ee00ab4e1
rickbacci <rickbacci@gmail.com>
2020-02-19 21:48:18 -0700

    install spec reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,7 @@ require 'pry'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require './lib/runner'
