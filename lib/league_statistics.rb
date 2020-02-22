142a24692d00814c6240beca52b8845121f0af2f
rickbacci <rickbacci@gmail.com>
2020-02-21 20:19:12 -0700

    best_offense spec passing

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -4,4 +4,42 @@ module LeagueStatistics
     teams.size
   end
 
+  def best_offense
+    # Name of the team with the highest average number of
+    # goals scored per game across all seasons
+    foo = {}
+
+    teams.each do |team|
+      # total number of games the team played(home or away)
+      total_team_games = games.select do |game|
+        game["away_team_id"] == team["team_id"] || game["home_team_id"] == team["team_id"]
+      end.size
+
+      away_games = games.select do |game|
+        game["away_team_id"] == team["team_id"]
+      end
+
+      average_goals_away = away_games.map do |game|
+        game["away_goals"].to_f
+      end.reduce(:+) / away_games.size
+
+
+      home_games = games.select do |game|
+        game["home_team_id"] == team["team_id"]
+      end
+
+      average_goals_home = home_games.map do |game|
+        game["home_goals"].to_f
+      end.reduce(:+) / home_games.size
+
+      highest_average = average_goals_home > average_goals_away ? average_goals_home.round(2) : average_goals_away.round(2)
+
+      team_name = team["teamName"]
+
+      foo[team_name] = highest_average
+    end
+
+    foo.sort_by { |k,v| -v }.first[0]
+  end
+
 end
