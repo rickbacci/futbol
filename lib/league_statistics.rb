df6cf24ca2493ffc9b2b4b15ae52c4a768c08212
rickbacci <rickbacci@gmail.com>
2020-03-02 18:23:48 -0700

    fix indentation

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -158,78 +158,78 @@ module LeagueStatistics
   private
 
 
-    def home_away_winning_percent_difference(team)
-      (away_games_win_percentage(team) - home_games_win_percentage(team))
-        .abs.round(2)
-    end
+  def home_away_winning_percent_difference(team)
+    (away_games_win_percentage(team) - home_games_win_percentage(team))
+      .abs.round(2)
+  end
 
-    def away_games_win_percentage(team)
-      total_away_games_won(team) / total_away_games_played(team)
-    end
+  def away_games_win_percentage(team)
+    total_away_games_won(team) / total_away_games_played(team)
+  end
 
-    def home_games_win_percentage(team)
-      total_home_games_won(team) / total_home_games_played(team)
-    end
+  def home_games_win_percentage(team)
+    total_home_games_won(team) / total_home_games_played(team)
+  end
 
-    def away_games(team)
-      games.select do |game|
-        game["away_team_id"] == team["team_id"]
-      end
+  def away_games(team)
+    games.select do |game|
+      game["away_team_id"] == team["team_id"]
     end
+  end
 
-    def total_away_games_played(team)
-      away_games(team).size.to_f
+  def total_away_games_played(team)
+    away_games(team).size.to_f
+  end
+
+  def home_games(team)
+    games.select do |game|
+      game["home_team_id"] == team["team_id"]
     end
+  end
 
-    def home_games(team)
-      games.select do |game|
-        game["home_team_id"] == team["team_id"]
-      end
-    end
+  def total_home_games_played(team)
+    home_games(team).size.to_f
+  end
 
-    def total_home_games_played(team)
-      home_games(team).size.to_f
-    end
+  def average_goals_away(team)
+    away_games(team).map do |game|
+      game["away_goals"].to_i
+    end.reduce(:+) / away_games(team).size.to_f
+  end
 
-    def average_goals_away(team)
-      away_games(team).map do |game|
-        game["away_goals"].to_i
-      end.reduce(:+) / away_games(team).size.to_f
-    end
+  def average_goals_home(team)
+    home_games(team).map do |game|
+      game["home_goals"].to_i
+    end.reduce(:+) / home_games(team).size.to_f
+  end
 
-    def average_goals_home(team)
-      home_games(team).map do |game|
-        game["home_goals"].to_i
-      end.reduce(:+) / home_games(team).size.to_f
-    end
+  def total_away_games_won(team)
+    away_games(team).select do |game|
+      game["away_goals"].to_f > game["home_goals"].to_f
+    end.size
+  end
 
-    def total_away_games_won(team)
-      away_games(team).select do |game|
-        game["away_goals"].to_f > game["home_goals"].to_f
-      end.size
-    end
+  def total_home_games_won(team)
+    home_games(team).select do |game|
+      game["home_goals"].to_f > game["away_goals"].to_f
+    end.size
+  end
 
-    def total_home_games_won(team)
-      home_games(team).select do |game|
-        game["home_goals"].to_f > game["away_goals"].to_f
-      end.size
-    end
-
-    def highest_average(team)
-      average_goals_home(team) > average_goals_away(team) ?
+  def highest_average(team)
+    average_goals_home(team) > average_goals_away(team) ?
       average_goals_home(team) : average_goals_away(team)
-    end
+  end
 
-    def total_games_won(team)
-      total_home_games_won(team) + total_away_games_won(team)
-    end
+  def total_games_won(team)
+    total_home_games_won(team) + total_away_games_won(team)
+  end
 
-    def total_games_played(team)
-      (away_games(team).size + home_games(team).size).to_f
-    end
+  def total_games_played(team)
+    (away_games(team).size + home_games(team).size).to_f
+  end
 
-    def winning_percent(team)
-      total_games_won(team) / total_games_played(team)
-    end
+  def winning_percent(team)
+    total_games_won(team) / total_games_played(team)
+  end
 
 end
